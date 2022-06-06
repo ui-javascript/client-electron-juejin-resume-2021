40e2013d284a7b70f94c9a4fde5aa24c7ac2d28c
luo0412 <1095847440@qq.com>
2022-06-06 14:19:42 +0800

    adjust

--- node_modules/eslint-config-alloy/scripts/testPrism.ts
+++
@@ -1,9 +0,0 @@
-import { ESLint } from 'eslint';
-
-const eslintInstance = new ESLint({});
-
-main();
-async function main() {
-  const results = await eslintInstance.lintFiles(['./test/index/no-cond-assign/bad.js']);
-  console.log(results[0].messages);
-}
